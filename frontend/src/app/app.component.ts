50701f02dcdd71c2d2126ae6d3eac0e34da483e5
Miguel Montes <mimopo@users.noreply.github.com>
2020-05-02 13:55:19 +0200

    Add recconnecting warning on disconnect

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+
+import { SeverityEnum } from 'src/app/notification/model/severity.enum';
+import { NotificationService } from 'src/app/notification/notification.service';
 import { Socket } from './services/socket';
 
 @Component({
@@ -8,9 +12,14 @@ import { Socket } from './services/socket';
 })
 export class AppComponent implements OnInit {
   // TODO: Better reconnection handling
-  constructor(private socket: Socket) {}
+  constructor(private socket: Socket, private notification: NotificationService) {}
 
   ngOnInit() {
-    this.socket.on('disconnect', () => location.reload());
+    this.socket.on('disconnect', () => {
+      this.notification.notify('Connection lost. Reconnecting...', SeverityEnum.danger);
+      timer(1000).subscribe(() => {
+        location.reload();
+      });
+    });
   }
 }
