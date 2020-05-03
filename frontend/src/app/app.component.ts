9e5d8485536d69713d1fc5d4ee99f5482bb5263b
Miguel Montes <mimopo@users.noreply.github.com>
2020-05-03 21:00:35 +0200

    Add connecting to server message

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,11 +11,20 @@ import { Socket } from './services/socket';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  connected = false;
+
   // TODO: Better reconnection handling
   constructor(private socket: Socket, private notification: NotificationService) {}
 
   ngOnInit() {
-    this.socket.on('disconnect', () => {
+    this.connected = this.socket.connected;
+    if (!this.connected) {
+      this.socket.on('connect', () => {
+        this.connected = true;
+      });
+    }
+    this.socket.once('disconnect', () => {
+      this.connected = false;
       this.notification.notify('Connection lost. Reconnecting...', SeverityEnum.danger);
       timer(1000).subscribe(() => {
         location.reload();
